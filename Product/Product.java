4ff880e4ae85d375e929c8143b1fb0274142fb16
Ronan Drost <ronandrost20@gmail.com>
2020-12-07 13:45:31 +0100

    Correct start

--- Product/Product.java
+++
@@ -1,8 +0,0 @@
-package Product;
-
-public class Product {
-    private int unitsInStock;
-    private float priceOfUnit;
-    private int itemNumber;
-    private String productName;
-}
---
+++ Products/src/main/java/Product/App.java
@@ -0,0 +1,23 @@
+package Product;
+
+/**
+ * Hello world!
+ */
+public final class Product {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        public class Product {
+            private int unitsInStock;
+            private float priceOfUnit;
+            private int itemNumber;
+            private String productName;
+        }
+    }
+}
